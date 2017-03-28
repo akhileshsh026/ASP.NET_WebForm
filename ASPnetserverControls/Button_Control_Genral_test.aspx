e8e73e56e3d971f43cc1781b194d4963511be1dd
Akhileshsh026@hotmail.com <Akhilesh@123>
2017-03-28 16:38:01 +0530

    genral lilnkage

---
+++ ASPnetserverControls/Button_Control_Genral_test.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Button_Control_Genral_test.aspx.cs" Inherits="ASPnetserverControls.Button_Control_Genral_test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div align="center" >
+            <asp:Button ID="Mybutton" runat="server"   Text="Submit Botton" />
+            <br />
+            <br />
+            <asp:Button ID="CommandButton" runat="server" Text="Command Button" CommandName="Insert" CommandArgument="1" OnCommand="CommandButton_Command"/>
+            <br />
+            <br />
+            <asp:Button ID=""
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPnetserverControls/Button_Control_Genral_test.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPnetserverControls
+{
+    public partial class Button_Control_Genral_test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Mybutton.Click += new EventHandler(Mybutton_click);
+            
+
+        }
+
+    
+
+        //protected void Mybutton_Click(object sender, EventArgs e)
+        //{
+        //    Mybutton.Text = "You just click me."; <asp:Button ID="Mybutton" runat="server" we have to add here onclick ="Mybutton_cick"  Text="Botton" />
+        //}
+
+        protected void Mybutton_click(object sender,EventArgs e)
+        {
+            Mybutton.Text = "You just click me.";
+        }
+
+        protected void CommandButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Write("the command name is : " + e.CommandName + "</b>");
+            Response.Write(" </br> The commnd Argument is </b> :"+e.CommandArgument+"</b>");
+        }
+    }
+}
